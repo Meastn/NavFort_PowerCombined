3f39ca772029d9ed6bb1c4343b24cf1537dcdfc6
Meastn <av.melihcalikoglu@protonmail.com>
2022-11-08 21:27:55 +0300

    beginning

---
+++ src/test/java/com/navfort/pages/Melih_Locators.java
@@ -0,0 +1,36 @@
+package com.navfort.pages;
+
+import com.navfort.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Melih_Locators {
+
+    public Melih_Locators(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//h2[@class='title']")
+    public WebElement loginPageTitle;
+
+    @FindBy(xpath = "//*[@id='prependedInput']")
+    public WebElement usernameInputbox;
+
+    @FindBy (name = "_password")
+    public WebElement passwordInputBox;
+
+    @FindBy (id="remember_me")
+    public  WebElement rememberMeCheckBox;
+
+    @FindBy (xpath = "//a[@href='/user/reset-request']")
+    public WebElement forgotPasswordLink;
+
+    @FindBy (id="_submit")
+    public WebElement loginButton;
+
+    @FindBy(name = "_password")
+    public WebElement passwordSpan;
+
+
+}
