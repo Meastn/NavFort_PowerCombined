de243ec0ceed109798e7755339c1a3a6c1d29aa0
metkaergul <metkaergul@gmail.com>
2022-11-14 12:07:07 -0500

    last updates

--- src/test/java/com/navfort/runners/CukesRunner.java
+++ src/test/java/com/navfort/runners/CukesRunner.java
@@ -17,8 +17,8 @@ import org.junit.runner.RunWith;
 
 
 
-            dryRun = true,
-            tags ="",
+            dryRun = false,
+            tags ="@Nav_800",
 
 
             publish = false
