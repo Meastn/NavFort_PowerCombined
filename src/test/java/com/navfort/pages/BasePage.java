ee213301543e66492c2f10e7c0bfb38391551f7b
Meastn <av.melihcalikoglu@protonmail.com>
2022-11-14 19:31:24 +0300

    Completed test design. Linking to JİRA today.

--- src/test/java/com/navfort/pages/BasePage.java
+++ src/test/java/com/navfort/pages/BasePage.java
@@ -7,39 +7,45 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
 
-   public BasePage() {
+    public BasePage() {
 
-      PageFactory.initElements(Driver.getDriver(), this);
-   }
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
 
+    @FindBy(xpath = "//h1[@class='oro-subtitle']")
+    public WebElement homePageTitle;
 
-   @FindBy(xpath = "//h1[@class='oro-subtitle']")
-   public WebElement homePageTitle;
-
-   @FindBy(xpath = "(//li[@class='dropdown dropdown-level-1'])[1]")
+    @FindBy(xpath = "(//li[@class='dropdown dropdown-level-1'])[1]")
     public WebElement fleetButton;
 
     @FindBy(xpath = "//a[@href='entity/Extend_Entity_Carreservation']")
-   public WebElement vehiclesButton;
+    public WebElement vehiclesButton;
 
-  
+
 // Melih's locators ...
 
-  @FindBy(xpath = "//*[@id='user-menu']/a/i")
+    @FindBy(xpath = "//*[@id='user-menu']/a/i")
     public WebElement userMenu;
 
- @FindBy(xpath = "//a[@class='no-hash']")
- public WebElement logoutMenuButton;
 
-    
+    @FindBy(xpath = "//i[@class='fa-caret-down']")
+    public WebElement userMenuDropDown;
+
+
+    @FindBy(xpath = "//a[@href='/user/logout']")
+    public WebElement logoutButton;
+
     // Melih's locators--
 
 
-   @FindBy(xpath = "//div[@class='bar']") // added by Serdar in order to use in BrowserUtils.invisibilityOf
-   public WebElement loadingBar;
+    @FindBy(xpath = "//div[@class='bar']") // added by Serdar in order to use in BrowserUtils.invisibilityOf
+    public WebElement loadingBar;
 
-   @FindBy(xpath = "(//div[@class='loader-mask shown']/div)[2]") //added by Serdar in order to use in BrowserUtils.invisibilityOf
-   public WebElement spinningWheel;
+    @FindBy(xpath = "(//div[@class='loader-mask shown']/div)[2]")
+    //added by Serdar in order to use in BrowserUtils.invisibilityOf
+    public WebElement spinningWheel;
+
+
 
 }
--- src/test/resources/features/NVF-794_General_Information_Page.feature
+++ src/test/resources/features/NVF-794_General_Information_Page.feature
@@ -1,13 +1,15 @@
   #As a user, I should be able to see detailed information
     # of a specific vehicle, under Fleet Vehicle module
-  @NVF-852
+  @NVF-847
   Feature: General Information Page Tests
+
     Background:
       Given User logs in to the application
 
 #1- User can see the "General Information" page by clicking
 # on any vehicle (row)
-    @NVF-847
+
+    @NVF-842
     Scenario: User can see the car general information by clicking a table row
       Given User clicks fleet and vehicles menu items
       When User clicks any row
@@ -15,7 +17,8 @@
 
 #2- User can see the "General Information" page by clicking
 # on the "Eye (View)" icon at the end of each row
-    @NVF-848
+
+    @NVF-843
     Scenario: User can see the car general information by clicking the view option
       Given User clicks fleet and vehicles menu items
       When User clicks on eye icon
@@ -23,7 +26,8 @@
 
 #3- Sales Manager and Store Manager should see "Edit",
 # "Delete" and "Add Event" buttons on the "General Information" page
-    @NVF-849
+
+    @NVF-844
     Scenario: Managers should see options on general information page
       Given User clicks fleet and vehicles menu items
       When User clicks any row
@@ -32,7 +36,8 @@
       And User can see the add event button
 
    #4- Driver shouldn't see "Add Event", "Edit" and "Delete" buttons
-    @NVF-850
+
+    @NVF-845
     Scenario: Driver shall not see options on general information page
       Given User logs out
       When User logs in as a Driver
@@ -44,7 +49,7 @@
 
 #5- Vehicle information displayed on the "General Information" page
 # and "Fleet-Vehicle" page should be the same
-    @NVF-851
+    @NVF-846
     Scenario: Vehicle information on general information and vehicle table should be the same
       Given User clicks fleet and vehicles menu items
       Then Data from the table and car page matches correctly
